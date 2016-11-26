593a4b7e3a61a96073b14e62ea5bac998c0275d7
nownabe <nownabe@gmail.com>
2016-11-26 23:56:41 +0900

    :speaker: Method call

--- lib/saira/virtual_machine.rb
+++ lib/saira/virtual_machine.rb
@@ -1,13 +1,58 @@
 module Saira
   class VirtualMachine
-    attr_reader :iseq
+    attr_reader :iseq, :stack, :main
 
     def initialize(iseq)
       @iseq = iseq
+      @stack = []
+      @main = generate_main
     end
 
     def run
-      puts iseq.disasm
+      iseq.to_a.last.each do |instruction|
+        next unless instruction.is_a?(Array)
+        execute(instruction)
+      end
+    end
+
+    def execute(instruction)
+      opecode = instruction.first
+      operand = instruction[1..-1]
+      $stderr.puts "==== #{opecode}(#{operand.map(&:inspect).join(', ')})"
+      case opecode
+      when :putself
+        push main
+      when :putstring
+        push operand[0]
+      when :send
+        call_info = operand[0]
+        args = Array.new(call_info[:orig_argc]) { pop }.reverse
+        receiver = pop
+        push receiver.send(call_info[:mid], *args)
+      when :leave
+      end
+      $stderr.puts "======== Stack: #{stack}"
+    end
+
+    def pop
+      stack.pop
+    end
+
+    def push(val)
+      stack.push(val)
+    end
+
+    private
+
+    def generate_main
+      main = Object.new
+      class << main
+        def to_s
+          "main"
+        end
+        alias inspect to_s
+      end
+      main
     end
   end
 end
